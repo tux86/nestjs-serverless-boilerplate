2be4b18306110049f39c2943459c6e2aa0db99a7
walid <walid.karray@gmail.com>
2022-04-15 13:29:31 +0200

    commit

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,10 @@
 module.exports = {
-  type: 'sqlite',
-  database: 'db.sqlite',
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
   entities: ['dist/**/*.entity.js'],
   synchronize: true,
 };
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmailTemplateModule } from './email-template/email-template.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot(),
     GraphQLModule.forRoot({ driver: ApolloDriver, autoSchemaFile: true }),
     EmailTemplateModule,
