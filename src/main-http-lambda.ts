47d355afbc55bd836240dd2ec40a2d9043e13fb9
walid <walid.karray@gmail.com>
2022-04-18 22:07:05 +0200

    refactor main and bootstrap

---
+++ src/main-http-lambda.ts
@@ -0,0 +1,13 @@
+import { Callback, Context, Handler } from 'aws-lambda';
+import { serverlessBootstrap as bootstrap } from './main.base';
+
+let cachedServer: Handler;
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  cachedServer = cachedServer ?? (await bootstrap());
+  return cachedServer(event, context, callback);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,3 @@
-import { NestApplication, NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { setupSwagger } from './main.common';
-import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
-
-const logger = new Logger(NestApplication.name);
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const configService: ConfigService = app.get(ConfigService);
-
-  // Swagger
-  setupSwagger(app);
-
-  const port = configService.get<number>('port');
-  await app.listen(port);
-  logger.log(`Application listening on port ${port}`);
-}
+import { defaultBootstrap as bootstrap } from './main.base';
 
 bootstrap();
