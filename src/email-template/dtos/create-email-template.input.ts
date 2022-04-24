2936ada6c4057d3418b3c2e274740526afb6ba05
walid <walid.karray@gmail.com>
2022-04-24 22:57:10 +0200

    eslint fix

--- src/email-template/dtos/create-email-template.input.ts
+++ src/email-template/dtos/create-email-template.input.ts
@@ -1,5 +1,5 @@
-import { Field, InputType } from "@nestjs/graphql";
-import { MinLength } from "class-validator";
+import { Field, InputType } from '@nestjs/graphql';
+import { MinLength } from 'class-validator';
 
 @InputType()
 export class CreateEmailTemplateInput {
